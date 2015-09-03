aafe73420c0be6dfdaefe8ad0b05ff811f3a2b1a
Kyle Frost <kylefrost@comcast.net>
2015-09-03 11:01:11 -0400

    Remove some color options, add footer w/ social links, add icon font, Google placeholder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import random
+import random, datetime
 
 app = Flask(__name__)
 
@@ -7,19 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     color_array = ["#B71C1C", # DARK RED
-                   "#4A148C", # DARK PURPLE
-                   "#1A237E", # DARK INDIGO
-                   "#0D47A1", # DARK BLUE
+                   #"#4A148C", # DARK PURPLE
+                   #"#1A237E", # DARK INDIGO
+                   #"#0D47A1", # DARK BLUE
                    "#006064", # DARK CYAN
                    "#009688", # TEAL
-                   "#1B5E20", # DARK GREEN
-                   "#FDD835", # MEDIUM YELLOW
-                   "#E65100", # DARK ORANGE
-                   "#3E2723", # DARK BROWN
+                   #"#1B5E20", # DARK GREEN
+                   #"#FDD835", # MEDIUM YELLOW
+                   #"#E65100", # DARK ORANGE
+                   #"#3E2723", # DARK BROWN
                    "#424242", # DARK GRAY
                    "#263238"] # DARK BLUE GRAY
-    color_choice = random.randrange(0, 11)
-    return render_template("index.html", color=color_array[color_choice], hovercolor="white")
+    color_choice = random.randrange(0, len(color_array) - 1)
+    return render_template("index.html", color=color_array[color_choice], hovercolor="white", year=datetime.datetime.now().year)
     
 # Run Flask app on load
 if __name__ == "__main__":
